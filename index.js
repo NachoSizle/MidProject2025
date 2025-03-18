ac0293ec57568e107d8a4a87eb7fe60bd0d40189
NachoSizle <NachoSizle@users.noreply.github.com>
2025-03-18 11:06:45 +0100

    Mod to works on Netlify deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = async () => {
   const projectsRes = await fetch(
-    "https://mid-project-nacho.netlify.app/projects"
+    "https://mid-project-nacho.netlify.app/api/projects"
   );
   const projects = await projectsRes.json();
   console.log(projects);
