4974f1e0d0d6439d5aca21182b05caccd045b0d4
NachoSizle <NachoSizle@users.noreply.github.com>
2025-03-18 12:05:15 +0100

    Mod JSON SERVER DB

--- functions/server.js
+++ functions/server.js
@@ -1,26 +1,17 @@
 const serverless = require("serverless-http");
 const jsonServer = require("json-server");
-
+const router = jsonServer.router("db.json");
 const app = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
-// Base de datos en memoria (carga estática)
-const db = {
-  projects: [
-    { id: 1, name: "Proyecto 1" },
-    { id: 2, name: "Proyecto 2" },
-  ],
-};
-
 // Cargar base de datos en memoria en JSON Server
-const router = jsonServer.router({ ...db });
 
 app.use(middlewares);
 app.use(router);
 
 console.log(
   "✅ JSON Server iniciado con los siguientes datos:",
-  JSON.stringify(db, null, 2)
+  JSON.stringify(router.db.getState(), null, 2)
 );
 
 module.exports.handler = serverless(app);
