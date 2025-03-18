fcb09810976268a40657f64dd332285ad1749695
NachoSizle <NachoSizle@users.noreply.github.com>
2025-03-18 11:31:51 +0100

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,6 @@
 const serverless = require("serverless-http");
 const jsonServer = require("json-server");
+
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
