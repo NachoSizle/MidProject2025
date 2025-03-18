f060eba7697515370bfecc421b5a7683fe4d9449
NachoSizle <NachoSizle@users.noreply.github.com>
2025-03-18 12:21:51 +0100

    Mod JSON SERVER DB

--- functions/server.js
+++ functions/server.js
@@ -8,32 +8,33 @@ const router = jsonServer.router({
 });
 const middlewares = jsonServer.defaults();
 
-// Asegurarse de que el servidor sirva las rutas correctas
 server.use(middlewares);
 server.use("/api", router);
 
 module.exports.handler = async (event, context) => {
   return new Promise((resolve, reject) => {
-    // Aquí usamos el método .handle para procesar la solicitud
+    // Usamos el método handle para procesar la solicitud
     server.handle(event, context, (err, response) => {
       if (err) {
         console.error("Error al manejar la solicitud:", err);
+        // Asegúrate de convertir el error a una cadena JSON
         return reject({
           statusCode: 500,
-          body: JSON.stringify({ message: "Error en el servidor" }),
+          body: JSON.stringify({ message: "Error en el servidor", error: err }),
         });
       }
 
-      // Asegúrate de que la respuesta esté correctamente formada
+      // Asegúrate de convertir la respuesta a una cadena JSON
       if (response) {
         return resolve({
           statusCode: response.statusCode || 200,
-          body: JSON.stringify(response.body),
+          body: JSON.stringify(response.body), // Convertir el cuerpo a JSON
           headers: {
             "Content-Type": "application/json",
           },
         });
       } else {
+        // Si no hay respuesta, devolver un error adecuado
         return reject({
           statusCode: 404,
           body: JSON.stringify({ message: "No se encontró la respuesta" }),
